a777c0499703c9e4d6243c03f5fe1bc8e99b1995
iwillseeyouinabits <isaaclsinger99@gmail.com>
2022-09-01 13:32:31 -0400

    running code for minning and file writer

--- src/FW.py
+++ src/FW.py
@@ -2,17 +2,19 @@ import portalocker
 
 class FW:
     def __init__(self, file):
-        file = open(file, "w+")
-        portalocker.lock(file)
+        file = open(file, "+r")
+        portalocker.lock(file, portalocker.LockFlags.EXCLUSIVE)
         self.file = file
     
     def write(self, text):
         while True:
             try:
+                self.file.truncate(0)
+                self.file.seek(0)
                 self.file.write(text)
+                break
             except Exception as e:
                 print(e)
-    
     
     def read(self):
         while True:
--- src/FileUpdater.py
+++ src/FileUpdater.py
@@ -1,9 +1,12 @@
 import json
+import time
+import random
 import FW
+import hashlib
 
 class FileUpdater:
 
-    def __init__(self, data):
+    def __init__(self, data="{\"type\":\"none\"}"):
         data = json.loads(data)
         if data["type"] == "block":
             self.block(data["data"])
@@ -42,5 +45,25 @@ class FileUpdater:
         fileBlock = FW.FW("block.json")
         block = json.loads(fileBlock.read())
         block["block"]["shell"].update(data)
-        fileBlock.write(block)
+        fileBlock.write(json.dumps(block, indent=4))
         fileBlock.close()
+
+    def updateBlock(self):
+        fileBlock = FW.FW("block.json")
+        block = json.loads(fileBlock.read())
+        block["block"]["timestamp"] = int(time.time())
+        block["block"]["nonce"] = random.randrange(1000000)
+        block["block_hash"] = hashlib.sha256(str(block["block"]).encode('utf-8')).hexdigest()
+        print(json.dumps(block, indent=4))
+        fileBlock.write(json.dumps(block, indent=4))
+        fileBlock.close()
+
+    def mine(self, numZeros):
+        while True:
+            self.updateBlock()
+            fileBlock = FW.FW("block.json") 
+            block = json.loads(fileBlock.read())
+            if(block["block_hash"][:numZeros] == numZeros*"0"):
+                break
+            fileBlock.close()
+
