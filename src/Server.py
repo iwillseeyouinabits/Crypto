cc9f21418d549722a9cf2cd50308ccd323155707
iwillseeyouinabits <isaaclsinger99@gmail.com>
2022-09-04 09:25:02 -0400

    Update to Server to not rely on call then response repetition

--- src/Server.py
+++ src/Server.py
@@ -3,62 +3,35 @@ import json
 
 class Server:
 
-    def __init__(self, ip, port, ip2, port2):
+    def __init__(self, ip, ip2):
         self.ip = ip
-        self.port = port
+        self.port = 42022
         self.ip2 = ip2
-        self.port2 = port2
+        self.port2 = 42022
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def receive(self):
-        self.socket.bind((self.ip, self.port))
-        self.socket.listen(5)
-        clientsocket, address = self.socket.accept()
-        while True:
+            print((self.ip, self.port))
+            self.socket.bind((self.ip, self.port))
+            self.socket.listen(500)
             while True:
-                msgOut = input("=> ")       
-                if msgOut == "CLOSE":
-                    self.socket.close()
-                    clientsocket.close()
-                    return
-                try:
-                    json.loads(msgOut)
-                    clientsocket.send(bytes(msgOut,"utf-8"))
-                    break
-                except:
-                    continue
-            msgIn = ""
-            while True:
-                msgIn += clientsocket.recv(1).decode("utf-8")
-                try:
-                    msgIn = json.loads(msgIn)
-                    msgIn = str(msgIn)
-                    break
-                except:
-                    continue
-            print(" => " + msgIn) 
+                clientsocket, address = self.socket.accept()
+                msgIn = ""
+                while True:
+                    msgIn += clientsocket.recv(1).decode("utf-8")
+                    try:
+                        msgIn = json.loads(msgIn)
+                        msgIn = str(msgIn)
+                        print(" => " + msgIn)
+                        break
+                    except:
+                        continue
 
-    def connect(self):
+    def connect(self, msg):
+        print((self.ip2, self.port2))
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((self.ip2, self.port2))
-        while True:
-            msgIn = ""
-            while True:
-                msgIn += self.socket.recv(1).decode("utf-8")
-                try:
-                    msgIn = json.loads(msgIn)
-                    msgIn = str(msgIn)
-                    break
-                except:
-                    continue
-            print(" => " + msgIn)
-            while True:
-                msgOut = input("=> ")
-                if msgOut == "CLOSE":
-                    self.socket.close()
-                    return
-                try:
-                    json.loads(msgOut)
-                    self.socket.send(bytes(msgOut, "utf-8"))
-                    break
-                except:
-                    continue
+        msgOut = msg
+        json.loads(msgOut)
+        self.socket.send(bytes(msgOut, "utf-8"))
+        self.socket.close()
