cca6638a4fe8548136cb2d72e9399dabd4841938
iwillseeyouinabits <isaaclsinger99@gmail.com>
2022-09-04 06:17:20 -0400

    First draft of handling data received from network

--- src/FileUpdater.py
+++ src/FileUpdater.py
@@ -9,45 +9,25 @@ from Verify import Verify
 
 class FileUpdater:
 
-    def __init__(self, data="{\"type\":\"none\"}"):
-        data = json.loads(data)
-        if data["type"] == "block":
-            self.block(data["data"])
-        elif data["type"] == "currency":
-            self.currency(data["type"])
-        elif data["type"] == "http":
-            self.http(data["data"])
-        elif data["type"] == "shell":
-            self.shell(data["data"])
-    
-    def block(self, data):
-        fileBlockChain = FW.FW("blockchain.json")
-        fileBlock = FW.FW("block.json")
-        blockchain = json.loads(fileBlockChain.read())
-        blockchain.update(data)
-        fileBlockChain.write(blockchain)
-        fileBlock.write("") #update this with empty block string
-        fileBlock.close()
-        fileBlockChain.close()
 
     def currency(self, data):
         fileBlock = FW.FW("block.json")
         block = json.loads(fileBlock.read())
-        block["block"]["currency"].update(data)
-        fileBlock.write(block)
+        block["block"]["currency"].append(data)
+        fileBlock.write(json.dumps(block, indent=4))
         fileBlock.close()
 
     def http(self, data):
         fileBlock = FW.FW("block.json")
         block = json.loads(fileBlock.read())
-        block["block"]["http"].update(data)
-        fileBlock.write(block)
+        block["block"]["http"].append(data)
+        fileBlock.write(json.dumps(block, indent=4))
         fileBlock.close()
 
     def shell(self, data):
         fileBlock = FW.FW("block.json")
         block = json.loads(fileBlock.read())
-        block["block"]["shell"].update(data)
+        block["block"]["shell"].append(data)
         fileBlock.write(json.dumps(block, indent=4))
         fileBlock.close()
 
@@ -167,3 +147,14 @@ class FileUpdater:
             fileBlockChain.write(json.dumps(blockChain, indent=4))
             fileBlockChain.close()
             self.makeNewBlock(selfN, selfE)
+
+    def handleNewInfo(self, selfN, selfE, msg):
+        data = json.loads(msg)
+        if data["type"] == "block":
+            self.addBlockToChain(selfN, selfE, data)
+        elif data["type"] == "currency":
+            self.currency(data["type"])
+        elif data["type"] == "http":
+            self.http(data["data"])
+        elif data["type"] == "shell":
+            self.shell(data["data"])
