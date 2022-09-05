861b7a91a8c5d6330561b44b7fe37de03247b4e0
Chris Fuentes <caf529@nyu.edu>
2022-09-05 16:51:03 -0400

    correcting ip

--- src/Server.py
+++ src/Server.py
@@ -1,13 +1,16 @@
 import socket
 import json
+import struct
+import fcntl
 import FileUpdater
 import json
-import FW
+
+from src import FW
 
 
 class Server:
     def __init__(self):
-        self.ip = socket.gethostbyname(socket.gethostname())
+        self.ip = self.get_ip_address()
         self.port = 42069
         self.ipDict = json.loads(FW.FW("IP.json").read())
         self.portOther = 42069
@@ -36,7 +39,15 @@ class Server:
         print("Connecting to ")
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         for ip in self.ipDict:
+
             print((ip, self.portOther))
             self.socket.connect((ip, self.portOther))
             self.socket.send(bytes(msgOut, "utf-8"))
             self.socket.close()
+    def get_ip_address(self, ifname):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(
+            s.fileno(),
+            0x8915,  # SIOCGIFADDR
+            struct.pack('256s', ifname[:15])
+        )[20:24])
